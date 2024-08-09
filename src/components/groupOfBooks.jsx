193a1cce5d9c8d0e0b4f4fb6366f1ccbb3d7c9c6
iMuhammadWali <mhdwali125@gmail.com>
2024-08-09 22:46:33 +0500

    Update groupOfBooks.jsx

--- src/components/groupOfBooks.jsx
+++ src/components/groupOfBooks.jsx
@@ -1,57 +1,10 @@
-// import { Link, useNavigate } from 'react-router-dom';
-// import '/src/App.css'
-
-// let category = document.querySelector("#category");
-// let num = 0;
-// let left = 0;
-// const GroupOfBooks = ({ category, books, bookCovers, isResult, query, setCurrBook }) => {
-
-//     return (
-//         <div className="category" key={category}>
-//             {!isResult ? (
-//                 <Link to={`/search/subject:${category}`} id="category">{category}</Link>
-//             ) : (
-//                 <span className="results">Search Results for "{query}"</span>
-//             )
-//             }
-//             <div className='sub-container' key={category}>
-//                 {books.map(book => {
-//                     num++;
-//                 (
-//                     <Link style={{
-//                         textDecoration: 'none'
-//                         }} to={`/details/${book.volumeInfo.title}`}
-//                     // }}
-//                         key={book.id}
-//                         className='book'
-//                         onClick={() => {
-//                             setCurrBook(book);
-//                             // console.log('current book is', currBook);
-//                             // navigate(`/details/${book.volumeInfo.title}`);
-//                         }}>
-//                         <img src={book.cover} />
-//                         <span id="title">{book.volumeInfo.title}</span>
-
-//                     </Link>
-//                 )
-//                 if (num == 1) {
-//                     left = document.querySelector(".book").getBoundingClientRect().x;
-                    
-//                 }})}
-//             </div>
-//         </div>
-//     )
-// };
-// // category.style.left = left + "px";
-// export default GroupOfBooks;
-
+import { Link } from 'react-router-dom';
 import React, { useEffect, useState, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import '/src/App.css';
 
 const GroupOfBooks = ({ category, books, bookCovers, isResult, query, setCurrBook }) => {
     const [left, setLeft] = useState(0);
-    const categoryRef = useRef(null);  // Reference for the category tag
+    const categoryRef = useRef(null);
 
     useEffect(() => {
         if (books.length > 0) {
@@ -64,36 +17,33 @@ const GroupOfBooks = ({ category, books, bookCovers, isResult, query, setCurrBoo
     }, [books]);
 
     useEffect(() => {
-        if (categoryRef.current) {  // Access the DOM element using categoryRef.current
+        if (categoryRef.current) {
             categoryRef.current.style.marginLeft = `${left}px`;
         }
     }, [left]);
-
     return (
-        <div className="category-container">
+        <div className="category" key={category}>
             {!isResult ? (
-                <Link to={`/search/subject:${category}`} id="category" ref={categoryRef} className="category">
-                    {category}
-                </Link>
+                <Link to={`/search/subject:${category}`} id="category" ref={categoryRef} >{category}</Link>
             ) : (
                 <span className="results">Search Results for "{query}"</span>
             )}
-            <div className='sub-container'>
+            <div className='sub-container' key={category}>
                 {books.map((book) => (
-                    <Link 
-                        style={{ textDecoration: 'none' }} 
-                        to={`/details/${book.volumeInfo.title}`}
+                    <Link style={{
+                        textDecoration: 'none'
+                    }} to={`/details/${book.volumeInfo.title}`}
                         key={book.id}
                         className='book'
-                        onClick={() => setCurrBook(book)}
-                    >
-                        <img src={bookCovers[book.id]} />
+                        onClick={() => {
+                            setCurrBook(book);
+                        }}>
+                        <img src={book.cover} />
                         <span id="title">{book.volumeInfo.title}</span>
                     </Link>
                 ))}
             </div>
         </div>
-    );
+    )
 };
-
 export default GroupOfBooks;
