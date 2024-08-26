b4686495028094802dc28458310034e6d1dd25b5
iMuhammadWali <mhdwali125@gmail.com>
2024-08-26 20:55:12 +0500

    Update nucesBooks.js

--- data/nucesBooks.js
+++ data/nucesBooks.js
@@ -22,7 +22,7 @@ const NUCES_Books = [
 
   {
     volumeInfo: {
-      title: "Model for writers",
+      title: "Model for writers - 12th Edition",
       imageLinks: {
         thumbnail: "https://prod-cat-files.macmillan.cloud/MediaResources/Jackets/258W/9781319214722.jpg",
       },
@@ -31,13 +31,13 @@ const NUCES_Books = [
         "First Semester"
       ]
     },
-    downloadLink: "https://ia601003.us.archive.org/30/items/modelsforwritersshortessaysforcompositionpdfdrive.com/Models%20for%20Writers_%20Short%20Essays%20for%20Composition%20%28%20PDFDrive.com%20%29.pdf" ,
+    downloadLink: "https://drive.google.com/file/d/10d9IICRyUxZkDT0mvp8C7A-BaPmRlXLT/view" ,
     isNuces: true
 
   },
   {
     volumeInfo: {
-      title: "Elementary Linear Algebra",
+      title: "Elementary Linear Algebra - 14th Edition",
       imageLinks: {
         thumbnail: "https://m.media-amazon.com/images/I/61JRg8KIRqL._SY425_.jpg",
       },
